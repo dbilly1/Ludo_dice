a7a81c396d1dbf3539cb1ba7a8c2b0c7799d958d
Douglas Billy <adzodid@gmail.com>
2021-07-07 11:37:06 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[200],
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue[200],
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
